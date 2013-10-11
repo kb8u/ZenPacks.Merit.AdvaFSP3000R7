1fa93ce23520fc65326901d90b748d23ffd5cfb1
Russell Dwarshuis <rjd@merit.edu>
2013-10-11 17:23:02 -0400

    Made device modeler run first so cache is complete when component modelers run

--- ZenPacks/Merit/AdvaFSP3000R7/lib/FSP3000R7MibCommon.py
+++ ZenPacks/Merit/AdvaFSP3000R7/lib/FSP3000R7MibCommon.py
@@ -7,97 +7,86 @@
 __doc__="""FSP3000R7MibCommon
 
 FSP3000R7MibCommon is a modeler base class to find components on an
-Adva FSP3000R7 system.  It reads previously collected SNMP data from 
-an Adva device model run from a file in /tmp so if there is more than
-one component to be modeled, the subsequent components will not have to
-get the same information over.  Without this, a system may respond so
-slowly that modeling times out in Zenoss.
+Adva FSP3000R7 system. It uses stored SNMP data from an Adva system in a
+file in /tmp so if there is more than one component to be modeled, the
+subsequent components will not have to get the same information over.
+Without this, a system may respond so slowly that modeling times out in
+Zenoss.  The stored SNMP data is created by the Adva device modeler which
+must be run first."""
 
-"""
-
-from Products.ZenModel.ZenPackPersistence import ZenPackPersistence
 from Products.DataCollector.plugins.CollectorPlugin import SnmpPlugin, GetTableMap, GetMap
-from Products.DataCollector.plugins.DataMaps import ObjectMap, RelationshipMap
+from Products.DataCollector.plugins.DataMaps import ObjectMap
 from ZenPacks.Merit.AdvaFSP3000R7.lib.FSP3000R7Channels import Channels
 import cPickle
 import time
+import os
 
 
+# Use SNMP data from Device Modeler in a cache file.  Can't be a PythonPlugin
+# since those run before any SnmpPlugin; device modeler is an PythonPlugin so
+# the cache file will be created before this is run.
 class FSP3000R7MibCommon(SnmpPlugin):
 
-    cache_file_name = '/tmp/%s.Adva_inventory_SNMP.pickle' % self.manageIp
-    # try to get snmp table from cache file first. Failing that, walk device
-    # and save to cache file.
-    snmpGetTableMaps = __read_cache()
-    if not snmpGetTableMaps:
-        inventoryTablecolumns = { '.1.1.': 'inventoryUnitName' }
-        entityTablecolumns = {
-            '.1.2.': 'entityContainedIn',
-            '.1.5.': 'entityIndexAid',
-            '.1.7.': 'entityAssignmentState',
-            '.1.8.': 'entityEquipmentState' }
-        opticalIfDiagcolumns = { '.1.3.': 'opticalIfDiagInputPower' }
-
-        # GetTableMap gets tabular data
-        snmpGetTableMaps = (
-            GetTableMap('inventoryTableEntry',
-                        '1.3.6.1.4.1.2544.2.5.5.1',
-                        inventoryTablecolumns),
-            GetTableMap('entityTableEntry',
-                        '1.3.6.1.4.1.2544.2.5.5.2',
-                        entityTablecolumns),
-            GetTableMap('opticalIfDiagTableEntry',
-                        '1.3.6.1.4.1.2544.1.11.2.4.3.5',
-                        opticalIfDiagcolumns) )
-        # save results to cache file
-        try:
-            cache_file = open(cache_file_name, 'w')
-            cache_file_time = time.time()
-            cPickle.dump(cache_file,cache_file_time)
-            cPickle.dump(cache_file,snmpGetTableMaps)
-            cache_file.close()
-        except IOError,cPickle.PickleError:
-            log.warn("Couldn't store SNMP data in " + cache_file_name)
+    # FspR7-MIB mib neSystemId is .1.3.6.1.4.1.2544.1.11.2.2.1.1.0.  Not used;
+    # Have to get something with SNMP or modeler won't process
+    snmpGetMap = GetMap({'.1.3.6.1.4.1.2544.1.11.2.2.1.1.0' : 'setHWTag'})
 
 
     def process(self, device, results, log):
-        log.info('processing  %s for device %s', self.name(), device.id)
+        """process snmp information for components from this device"""
+        log.info('processing %s for device %s', self.name(), device.id)
 
-        if not results['inventoryTable']:
-            log.warn(
-                'No SNMP inventoryTable response from %s for the %s plugin',
-                device.id, self.name() )
+        # tabledata is not used (get tables from cache pickle file created
+        # in FSP3000R7Device modeler)
+        getdata = {}
+        getdata['setHWTag'] = False
+        getdata, tabledata = results
+        if not getdata['setHWTag']:
+            log.info("Couldn't get system name from Adva shelf.")
+
+        cache_file_name = '/tmp/%s.Adva_inventory_SNMP.pickle' % device.id
+
+        inventoryTable = entityTable = opticalIfDiagTable = False
+        cache_file_time = 0
+
+        bad_cache = 0
+
+        try:
+            cache_file = open(cache_file_name, 'r')
+            inventoryTable = cPickle.load(cache_file)
+            entityTable = cPickle.load(cache_file)
+            opticalIfDiagTable = cPickle.load(cache_file)
+            cache_file_time = cPickle.load(cache_file)
+            cache_file.close()
+        except IOError,cPickle.PickleError:
+            log.debug('Could not open or read %s', cache_file_name)
+            bad_cache = 1
+
+        if bad_cache or cache_file_time < time.time() - 900:
+            log.warn("Cached SNMP doesn't exist or is older than 15 minutes. You must include the modeler plugin FSP3000R7Device")
             return
-        if not results['entityTable']:
-            log.warn(
-                'No SNMP entityTable response from %s for the %s plugin',
-                device.id, self.name() )
-            return
+
+        if not inventoryTable:
+            log.warn('No SNMP inventoryTable response from %s for %s',
+                     device.id, self.name())
+            return;
+        if not entityTable:
+            log.warn('No SNMP entityTable response from %s for the %s plugin',
+                     device.id, self.name())
+            return;
         else:
             log.debug('SNMP entityTable and inventoryTable responses received')
-        # not all modules will respond to opticalIfDiagTable so don't return 
-        if not results['opticalIfDiagTable']:
+        # not all modules will respond to opticalIfDiagTable so don't return
+        if not opticalIfDiagTable:
             log.warn(
-                'No SNMP opticalIfDiagTable response from %s for the %s plugin',
-                device.id, self.name() )
+              'No SNMP opticalIfDiagTable response from %s for the %s plugin',
+              device.id, self.name())
         else:
             log.debug(
                 'SNMP opticalIfDiagTable and inventoryTable responses received')
 
-        # nothing to process if collect didn't has a problem
-        if results is False:
-            return
-        log.info('processing %s for device %s', self.name(), device.id)
-
-        inventoryTable = results['inventoryTable']
-        entityTable = results['entityTable']
-        opticalIfDiagTable = results['opticalIfDiagTable']
-
-        # relationship mapping
-        rm = self.relMap()
-
         # dictionary of lists of what submodules are contained
-        # in modules & submodules.  Use strings to avoid key errors.
+        # in modules & submodules. Use strings to avoid key errors.
         containsModules = {}
         for entityIndex, entityContainedIn in entityTable.items():
             entityIndex_str = str(entityIndex)
@@ -106,67 +95,70 @@ class FSP3000R7MibCommon(SnmpPlugin):
                 containsModules[entityContainedIn_str] = []
             containsModules[entityContainedIn_str].append(entityIndex_str)
 
+        # relationship mapping
+        rm = self.relMap()
+
         for entityIndex, inventoryUnitName in inventoryTable.items():
-          entityIndex_str = str(entityIndex)
-          if self.__model_match(inventoryUnitName['inventoryUnitName'],
-                          self.componentModels) \
-          and entityIndex in entityTable \
-          and 'entityAssignmentState' in entityTable[entityIndex] \
-          and 'entityEquipmentState' in entityTable[entityIndex] \
-          and entityTable[entityIndex]['entityAssignmentState'] == 1 \
-          and entityTable[entityIndex]['entityEquipmentState'] == 1:
-              om = self.objectMap()
-              om.EntityIndex = int(entityIndex)
-              om.inventoryUnitName = inventoryUnitName['inventoryUnitName']
-              om.entityIndexAid = entityTable[entityIndex]['entityIndexAid']
-              om.sortKey = self.__make_sort_key(om.entityIndexAid)
-              om.entityAssignmentState = \
-                  entityTable[entityIndex]['entityAssignmentState']
-              om.id = self.prepId(om.entityIndexAid)
-              om.title = om.entityIndexAid
-              om.snmpindex = int(entityIndex)
-              log.info('Found component at: %s inventoryUnitName: %s',
-                  om.entityIndexAid, om.inventoryUnitName)
+            entityIndex_str = str(entityIndex)
+            if self.__model_match(inventoryUnitName['inventoryUnitName'],
+                            self.componentModels) \
+              and entityIndex in entityTable \
+              and 'entityAssignmentState' in entityTable[entityIndex] \
+              and 'entityEquipmentState' in entityTable[entityIndex] \
+              and entityTable[entityIndex]['entityAssignmentState'] == 1 \
+              and entityTable[entityIndex]['entityEquipmentState'] == 1:
+                om = self.objectMap()
+                om.EntityIndex = int(entityIndex)
+                om.inventoryUnitName = inventoryUnitName['inventoryUnitName']
+                om.entityIndexAid = entityTable[entityIndex]['entityIndexAid']
+                om.sortKey = self.__make_sort_key(om.entityIndexAid)
+                om.entityAssignmentState = \
+                    entityTable[entityIndex]['entityAssignmentState']
+                om.id = self.prepId(om.entityIndexAid)
+                om.title = om.entityIndexAid
+                om.snmpindex = int(entityIndex)
+                log.info('Found component at: %s inventoryUnitName: %s',
+                         om.entityIndexAid, om.inventoryUnitName)
 
-              rm.append(om)
+                rm.append(om)
 
-              # Now find sub-organizers that respond to OPR
-              if not opticalIfDiagTable:
-                  continue
-              if entityIndex_str not in containsModules:
-                  continue
-              # EntityIndex's with valid responses to opticalIfDiagTable
-              opr_responders = []
-              self.__get_opr_responders(opr_responders,
-                                        entityIndex_str,
-                                        containsModules,
-                                        opticalIfDiagTable)
+                # Now find sub-organizers that respond to OPR
+                if not opticalIfDiagTable:
+                    continue
+                if entityIndex_str not in containsModules:
+                    continue
+                # EntityIndex's with valid responses to opticalIfDiagTable
+                opr_responders = []
+                self.__get_opr_responders(opr_responders,
+                                          entityIndex_str,
+                                          containsModules,
+                                          opticalIfDiagTable)
 
-              for entityIndex in opr_responders:
-                  # skip non-production components.  entityEquipmentState is 0
-                  # (undefined) for sub-organizers so don't check it here.
-                  if not (entityIndex in entityTable \
-                    and 'entityAssignmentState' in entityTable[entityIndex] \
-                    and entityTable[entityIndex]['entityAssignmentState']==1):
-                      continue;
-                  om = self.objectMap()
-                  om.EntityIndex = int(entityIndex)
-                  if entityIndex in inventoryUnitName:
-                      om.inventoryUnitName = \
-                          inventoryUnitName[entityIndex]['inventoryUnitName']
-                  else:
-                      om.inventoryUnitName = 'Subsystem'
-                  om.entityIndexAid=entityTable[entityIndex]['entityIndexAid']
-                  om.sortKey = self.__make_sort_key(om.entityIndexAid)
-                  om.entityAssignmentState = \
-                      entityTable[entityIndex]['entityAssignmentState']
-                  om.id = self.prepId(om.entityIndexAid)
-                  om.title = om.entityIndexAid
-                  om.snmpindex = int(entityIndex)
-                  log.info('Found component at: %s inventoryUnitName: %s',
-                      om.entityIndexAid, om.inventoryUnitName)
+                for entityIndex in opr_responders:
+                    # skip non-production components. entityEquipmentState is 0
+                    # (undefined) for sub-organizers so don't check it here.
+                    if not (entityIndex in entityTable \
+                      and 'entityAssignmentState' in entityTable[entityIndex] \
+                      and entityTable[entityIndex]['entityAssignmentState']==1):
+                        continue;
+                    om = self.objectMap()
+                    om.EntityIndex = int(entityIndex)
+                    if entityIndex in inventoryUnitName:
+                        om.inventoryUnitName = \
+                            inventoryUnitName[entityIndex]['inventoryUnitName']
+                    else:
+                        om.inventoryUnitName = 'Subsystem'
+                    om.entityIndexAid=entityTable[entityIndex]['entityIndexAid']
+                    om.sortKey = self.__make_sort_key(om.entityIndexAid)
+                    om.entityAssignmentState = \
+                        entityTable[entityIndex]['entityAssignmentState']
+                    om.id = self.prepId(om.entityIndexAid)
+                    om.title = om.entityIndexAid
+                    om.snmpindex = int(entityIndex)
+                    log.info('Found component at: %s inventoryUnitName: %s',
+                             om.entityIndexAid, om.inventoryUnitName)
 
-                  rm.append(om)
+                    rm.append(om)
 
         return rm
 
@@ -175,7 +167,7 @@ class FSP3000R7MibCommon(SnmpPlugin):
             # Test different channel variations if there's a # on end
             if model.endswith('#'):
                 all_ch = Channels.dwdm_old_channels+Channels.cwdm_channels
-             	for ch in all_ch:
+                for ch in all_ch:
                     model_var = model + ch
                     if inventoryUnitName == model_var:
                         return True
@@ -184,9 +176,12 @@ class FSP3000R7MibCommon(SnmpPlugin):
         return False
 
 
-    def __get_opr_responders(self,opr_responders,entityIndex_str,containsModules,opticalIfDiagTable):
-        """recursively walk containsModules looking for matches
-           in opticalIfDiagTable"""
+    def __get_opr_responders(self,
+                             opr_responders,
+                             entityIndex_str,
+                             containsModules,
+                             opticalIfDiagTable):
+        """recursively walk containsModules looking for matches in opticalIfDiagTable"""
         if entityIndex_str not in containsModules:
             return
         for entityIndex2 in containsModules[entityIndex_str]:
@@ -202,29 +197,10 @@ class FSP3000R7MibCommon(SnmpPlugin):
 
     def __make_sort_key(self,entityIndexAid):
         """Return a string to sort on, e.g. 'MOD-1-3' -> '001003000000'
-           and 'VCH-1-7-N1' -> '  1  7VCH N1'"""
+and 'VCH-1-7-N1' -> ' 1 7VCH N1'"""
         a = entityIndexAid.split('-',4)
         if len(a) < 3:
             return '000000000'
-        if entityIndexAid.startswith('MOD-') \
-        or entityIndexAid.startswith('FAN-'):
+        if entityIndexAid.startswith('MOD-') or entityIndexAid.startswith('FAN-'):
             return "%03s%03s000000" % (a[1],a[2])
         return "%03s%03s%03s%03s" % (a[1],a[2],a[0],a[3])
-
-
-    def __read_cache(self):
-        """Try to read cache file of snmp table walk.  Return False on fail"""
-        cache_file_time = 0
-        try:
-            cache_file = open(self.cache_file_name, 'w')
-            snmpGetTableMaps = cPickle.load(cache_file)
-            cache_file_time = cPickle.load(cache_file)
-            cache_file.close()
-        except IOError,cPickle.PickleError:
-            pass
-
-        if cache_file_time < time.time() - 900:
-            log.info("Cache file is missing or is not current.")
-            return False
-
-        return snmpGetTableMaps
--- ZenPacks/Merit/AdvaFSP3000R7/modeler/plugins/Adva/FSP3000R7DeviceMib.py
+++ ZenPacks/Merit/AdvaFSP3000R7/modeler/plugins/Adva/FSP3000R7DeviceMib.py
@@ -11,45 +11,160 @@
 
 __doc__="""FSP3000R7DeviceMib
 
-FSP3000R7DeviceMib gets System name from the NCU-II
+FSP3000R7DeviceMib gets System name from the NCU-II.  It creates a pickle
+file with information needed by component modelers so the same information
+does not have to be queried from the shelf over and over.  The Adva shelves
+can respond very slowly and without this strategy zenoss can time out before
+all the SNMP information is returned."""
 
-"""
 
-from Products.DataCollector.plugins.CollectorPlugin import SnmpPlugin, GetTableMap, GetMap
+from Products.DataCollector.plugins.CollectorPlugin import PythonPlugin
 from Products.DataCollector.plugins.DataMaps import ObjectMap
+import cPickle
+import time
+import subprocess
+from pprint import pformat
 
 
-class FSP3000R7DeviceMib(SnmpPlugin):
+class FSP3000R7DeviceMib(PythonPlugin):
+    """Use a PythonPlugin instead of SnmpPlugin since they run first in
+       def collectDevice in /opt/zenoss/Products/DataCollector/zenmodeler.py
+       Need that order so the pickle file will be created first."""
 
     modname = "ZenPacks.Merit.AdvaFSP3000R7.FSP3000R7Device"
 
-    # FspR7-MIB mib neSystemId is .1.3.6.1.4.1.2544.1.11.2.2.1.1.0
-    # FspR7-MIB mib neSwVersion is .1.3.6.1.4.1.2544.1.11.2.2.1.5.0
-    # it's the name and software version of the shelf
-    snmpGetMap = GetMap({
-        '.1.3.6.1.4.1.2544.1.11.2.2.1.1.0' : 'setHWTag',
-        '.1.3.6.1.4.1.2544.1.11.2.2.1.5.0' : 'setOSProductKey',
-    })
+
+    def copyDataToProxy(self, device, proxy):
+        """add zSnmpCommunity to device proxy"""
+        result = PythonPlugin.copyDataToProxy(self, device, proxy)
+        proxy.zSnmpCommunity = device.zSnmpCommunity
+
+
+    def collect(self, device, log):
+        log.info('Starting to collect SNMP data using snmpget and snmpbulkwalk')
+        # from the FspR7-MIB mib, neSystemId has the name of the system
+        # and neSwVersion is the software version
+        neSystemId = '.1.3.6.1.4.1.2544.1.11.2.2.1.1.0'
+        neSwVersion = '.1.3.6.1.4.1.2544.1.11.2.2.1.5.0'
+
+        inventoryUnitNameOID = '1.3.6.1.4.1.2544.2.5.5.1.1.1'
+        entityContainedInOID = '1.3.6.1.4.1.2544.2.5.5.2.1.2'
+        entityIndexAidOID = '1.3.6.1.4.1.2544.2.5.5.2.1.5'
+        entityAssignmentStateOID = '1.3.6.1.4.1.2544.2.5.5.2.1.7'
+        entityEquipmentStateOID = '1.3.6.1.4.1.2544.2.5.5.2.1.8'
+        opticalIfDiagInputPowerOID = '1.3.6.1.4.1.2544.1.11.2.4.3.5.1.3'
+
+        getdata = {}
+        
+        self.__snmpget(device,neSystemId,'setHWTag',getdata)
+        log.info('Got system name %s' % getdata['setHWTag'])
+        self.__snmpget(device,neSwVersion,'setOSProductKey',getdata)
+        log.info('Got software version %s' % getdata['setOSProductKey'])
+
+        inventoryTable = {}
+        raw_inventory = {}
+        raw_inventory = self.__snmpgettable(device,inventoryUnitNameOID)
+        self.__make_cacheable('inventoryUnitName',raw_inventory,inventoryTable)
+        log.debug('inventoryTable: %s' % pformat(inventoryTable))
+
+        entityTable = {}
+        raw_entityContainedIn = {}
+        raw_entityContainedIn = self.__snmpgettable(device,entityContainedInOID)
+        self.__make_cacheable('entityContainedIn',
+                              raw_entityContainedIn,
+                              entityTable)
+
+        raw_entityIndexAid = {}
+        raw_entityIndexAid = self.__snmpgettable(device,entityIndexAidOID)
+        self.__make_cacheable('entityIndexAid',
+                              raw_entityIndexAid,
+                              entityTable)
+
+        raw_entityAssignmentState = {}
+        raw_entityAssignmentState = self.__snmpgettable(device,
+                                                       entityAssignmentStateOID)
+        self.__make_cacheable('entityAssignmentState',
+                              raw_entityAssignmentState,
+                              entityTable)
+
+        raw_entityEquipmentState = {}
+        raw_entityEquipmentState = self.__snmpgettable(device,
+                                                       entityEquipmentStateOID)
+        self.__make_cacheable('entityEquipmentState',
+                              raw_entityEquipmentState,
+                              entityTable)
+        log.debug('entityTable: %s' % pformat(entityTable))
+
+        opticalIfDiagTable = {}
+        raw_opticalIfDiagInputPower = {}
+        raw_opticalIfDiagInputPower= self.__snmpgettable(device,
+                                                     opticalIfDiagInputPowerOID)
+        self.__make_cacheable('opticalIfDiagInputPower',
+                              raw_opticalIfDiagInputPower,
+                              opticalIfDiagTable)
+        log.debug('opticalIfDiagTable: %s' % pformat(opticalIfDiagTable))
+
+        cache_file_name = '/tmp/%s.Adva_inventory_SNMP.pickle' % device.id
+        try:
+            cache_file = open(cache_file_name,'w')
+            cPickle.dump(inventoryTable,cache_file)
+            cPickle.dump(entityTable,cache_file)
+            cPickle.dump(opticalIfDiagTable,cache_file)
+            cPickle.dump(time.time(),cache_file)
+            cache_file.close()
+        except IOError,cPickle.PickleError:
+            log.warn("Couldn't cache SNMP data in", cache_file_name)
+
+        return getdata
 
 
     def process(self, device, results, log):
-        """
-        collect snmp information from this device, set serial number,
-        and write cached SNMP data for subsequent component modeling in a
-        pickle file.
-        """
+        """Set the system name and software version"""
 
-        log.info('Retrieving Adva system title using %s for device %s',
-                 self.name(), device.id)
-
-        # Collect tag and serial number 
-        getdata = {}
-        getdata['setHWTag'] = False
-        getdata['setOSProductKey'] = 'Unknown'
-        getdata, tabledata = results
-        if not getdata['setHWTag']:
-            log.info("Couldn't get system name from Adva shelf.")
-            return
-
-        om = self.objectMap(getdata)
+        log.debug('in process, got results:',results)
+        rm = self.relMap()
+        om = self.objectMap(results)
         return om
+
+
+    def __snmpget(self,device,oid,oid_name,results):
+        cmd = ['snmpget','-v2c','-t','3',
+               '-c',device.zSnmpCommunity, '-Onq', device.manageIp,oid]
+        try:
+            results[oid_name] = \
+                subprocess.check_output(cmd).split(' ')[1].strip('\n"')
+        except subprocess.CalledProcessError, IndexError:
+            results[oid_name] = ''
+
+
+    def __snmpgettable(self,device,oid):
+        """Use snmpbulkwalk to get data.  Use timeout of 5 seconds since
+           shelves can be very slow to respond"""
+        cmd = ['snmpbulkwalk','-v2c','-t','5',
+               '-c',device.zSnmpCommunity, '-Onq', device.manageIp,oid]
+        results = {}
+        try:
+            walk_output = subprocess.check_output(cmd).split('\n')
+            for line in walk_output:
+                try:
+                    oid,val = line.split(' ',1)
+                    oid = oid.lstrip('.')
+                    val = val.strip('"')
+                except ValueError:
+                    continue
+                try:
+                    val = int(val)
+                except ValueError:
+                    pass
+                results[oid] = val
+        except subprocess.CalledProcessError, IndexError:
+            pass
+        return results
+
+
+    def __make_cacheable(self,name,raw,results):
+        for oid,val in raw.items():
+            index = oid.split('.')[-1]
+            if index not in results:
+                results[index] = {}
+            results[index][name] = val
