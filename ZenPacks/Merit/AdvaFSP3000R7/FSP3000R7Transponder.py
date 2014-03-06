d59c7478a99bd46800f30eb44d2f3ccd9c18ac92
Russell Dwarshuis <rjd@merit.edu>
2014-03-06 17:03:27 -0500

    fixed key error in GUI somehow...

--- ZenPacks/Merit/AdvaFSP3000R7/FSP3000R7Transponder.py
+++ ZenPacks/Merit/AdvaFSP3000R7/FSP3000R7Transponder.py
@@ -2,7 +2,7 @@
 #
 # FSP3000R7Transponder object class
 #
-# Copyright (C) 2011 Russell Dwarshuis, Merit Network, Inc.
+# Copyright (C) 2014 Russell Dwarshuis, Merit Network, Inc.
 #
 # This program can be used under the GNU General Public License version 2
 # You can find full information here: http://www.zenoss.com/oss
@@ -15,91 +15,19 @@ FSP3000R7Transponder is a component of a FSP3000R7Device Device
 
 """
 
-from Globals import DTMLFile
-from Globals import InitializeClass
-
-from Products.ZenRelations.RelSchema import *
-from Products.ZenModel.ZenossSecurity import ZEN_CHANGE_DEVICE
-from Products.ZenModel.ZenPackPersistence import ZenPackPersistence
-from Products.ZenModel.DeviceComponent import DeviceComponent
-from Products.ZenModel.ManagedEntity import ManagedEntity
+from ZenPacks.Merit.AdvaFSP3000R7.lib.FSP3000R7Component import *
 
 import logging
 log = logging.getLogger('FSP3000R7Transponder')
 
-class FSP3000R7Transponder(DeviceComponent, ManagedEntity, ZenPackPersistence):
+class FSP3000R7Transponder(FSP3000R7Component):
     """FSP3000R7Transponder object"""
 
     portal_type = meta_type = 'FSP3000R7Transponder'
-
-    # set default _properties
-    EntityIndex           = -1
-    inventoryUnitName     = 'Not set by modeler'
-    entityIndexAid        = 'Not set by modeler'
-    interfaceConfigId     = ''
-    sortKey               = '000000000'
-    entityAssignmentState = 'Not set by modeler'
-
-    _properties = (
-        # from ADVA inventory entityTable
-        {'id':'EntityIndex',           'type':'int',    'mode':''},
-        # from ADVA inventory MIB
-        {'id':'inventoryUnitName',     'type':'string', 'mode':''},
-        # human readable physical location of Transponder in shelf
-        {'id':'entityIndexAid',        'type':'string', 'mode':''},
-        # link comments (interfaceConfigIdentifier)
-        {'id':'interfaceConfigId',     'type':'string', 'mode':''},
-        # string used to sort names in GUI
-        {'id':'sortKey',               'type':'string', 'mode':''},
-        {'id':'entityAssignmentState', 'type':'string', 'mode':''},
-        )
 
     _relations = (
         ("FSP3000R7Dev", ToOne(ToManyCont,
              "ZenPacks.Merit.AdvaFSP3000R7.FSP3000R7Device", "FSP3000R7Trans")),
         )
-
-    factory_type_information = ({
-        'actions': ({
-            'id': 'perfConf',
-            'name': 'Template',
-            'action': 'objTemplates',
-            'permissions': (ZEN_CHANGE_DEVICE,),
-            },),
-        },)
-
-    isUserCreatedFlag = True
-
-    def isUserCreated(self):
-        """
-        Returns the value of isUserCreated. True adds SAVE & CANCEL buttons to Details menu
-        """
-        return self.isUserCreatedFlag
-
-    def viewName(self):
-        """Human readable version of this object"""
-        if self.inventoryUnitName == 'Not set by modeler' or self.EntityIndex == '-1':
-            return "Unknown"
-        return(self.entityIndexAid)
-
-    name = viewName
-
-    def primarySortKey(self):
-        return self.sortKey
-
-    def device(self):
-        return self.FSP3000R7Dev()
-
-    def monitored(self):
-        """Monitor transponder if it's provisioned (same thing as assigned)"""
-        if self.entityAssignmentState:
-            return True
-        else:
-            return False
-        return True
-
-    def manage_deleteComponent(self):
-        self.getPrimaryParent()._delObject(self.id)
-
 
 InitializeClass(FSP3000R7Transponder)
